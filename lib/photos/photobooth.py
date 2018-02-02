5e22f6d5b96bba8e376f0655daf94231d18d5bf8
kmhoran <kevin.michael.horan@gmail.com>
2018-02-02 15:31:43 -0800

    add photo-capture logic

--- lib/photos/photobooth.py
+++ lib/photos/photobooth.py
@@ -8,7 +8,32 @@ import os
 
 
 def run():
-    pass
+    import cv2
+    from datetime import datetime
+    from pathlib import Path
+    import os
 
-if __name__ == "__main__":
-    run()
+    camera = cv2.VideoCapture(0)
+
+    while (True):
+        try:
+            return_value, image = camera.read()
+
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+            homeDir = str(Path.home())
+            pictureDir = homeDir + "/Pictures/gitPics"
+            if not os.path.isdir(pictureDir):
+                os.makedirs(pictureDir)
+            now = datetime.now()
+            name = '{h}/capture-{t.year}-{t.month}-{t.day}_{t.hour}-{t.minute}-{t.second}.jpg'.format(h=pictureDir,
+                                                                                                      t=now)
+            print("saving ", name)
+            cv2.imwrite(name, image)
+            break
+        except:
+            print("exiting...")
+            break
+
+    camera.release()
+    cv2.destroyAllWindows()
